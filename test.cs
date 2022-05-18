fdfbf03a12ca757db0b50d2cfc3ce5c5ee5dbb20
MrJonesss <71847653+MrJonesss@users.noreply.github.com>
2022-05-18 19:18:43 +0200

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,38 +1,13 @@
 using System;
 
-namespace Kauwgom2
+namespace program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            KauwgomAutomaat kauwgomAutomaat1 = new KauwgomAutomaat("rood");
-            KauwgomAutomaat kauwgomAutomaat2 = new KauwgomAutomaat("rood", 50);
-
-            PrintKauwgoms(kauwgomAutomaat1, kauwgomAutomaat2);
-
-            kauwgomAutomaat1.Vergrendeld = false;
-
-            kauwgomAutomaat1.VulBij(20);
-
-            kauwgomAutomaat1.Vergrendeld = true;
-
-            PrintKauwgoms(kauwgomAutomaat1, kauwgomAutomaat2);
-
-            kauwgomAutomaat1.VulBij(75);
-
-            PrintKauwgoms(kauwgomAutomaat1, kauwgomAutomaat2);
+            console.writeline("Hello World");
         }
 
-        private static void PrintKauwgoms(KauwgomAutomaat kauwgom1, KauwgomAutomaat kauwgom2)
-        {
-            PrintKauwgom(kauwgom1);
-            PrintKauwgom(kauwgom2);
-        }
-
-        private static void PrintKauwgom(KauwgomAutomaat kauwgomAutomaat)
-        {
-            Console.WriteLine(kauwgomAutomaat.Kleur + " " + kauwgomAutomaat.Aantal);
-        }
     }
 }
